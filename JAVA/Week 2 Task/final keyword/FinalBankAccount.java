0f0af220f5f22423426dca25dc2add82c74df97f
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-11 16:15:16 +0530

    Create FinalBankAccount.java

---
+++ JAVA/Week 2 Task/final keyword/FinalBankAccount.java
@@ -0,0 +1,31 @@
+package org.example.assignment.finalexp;
+
+public class FinalBankAccount {
+    private final String accountNumber;
+    private double balance;
+
+    public FinalBankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
