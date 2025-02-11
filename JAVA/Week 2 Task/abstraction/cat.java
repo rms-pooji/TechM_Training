dafccb1bbf74f0add0debf2382ae5a2ed6e9f640
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-11 15:48:58 +0530

    Create cat.java

---
+++ JAVA/Week 2 Task/abstraction/cat.java
@@ -0,0 +1,8 @@
+package org.example.assignment.abstracteg;
+
+public class Cat extends Animal{
+    @Override
+    void makeSound(){
+        System.out.println("The Cat goes - Meow...Meow...!");
+    }
+}
