f433ddc96867acb7e63cfa797769ce50d874398f
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-11 15:50:02 +0530

    Create test.java

---
+++ JAVA/Week 2 Task/abstraction/test.java
@@ -0,0 +1,11 @@
+package org.example.assignment.abstracteg;
+
+public class Testm {
+    public static void main(String[] args){
+        Dog d=new Dog();
+        Cat o=new Cat();
+
+        d.makeSound();
+        o.makeSound();
+    }
+}
