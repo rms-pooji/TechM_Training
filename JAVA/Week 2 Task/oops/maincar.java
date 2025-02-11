33f1c8eaefdbbec863d9db36ea76c0fa3b3366fc
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-11 15:19:55 +0530

    Create maincar.java

---
+++ JAVA/Week 2 Task/oops/maincar.java
@@ -0,0 +1,19 @@
+package org.example.assignment.oopeg;
+
+import java.util.ArrayList;
+
+public class Mainc {
+    public static void main(String[] args){
+        ArrayList<Car> cars= new ArrayList<>();
+
+        cars.add(new Car("Toyota", "Camry", 2022));
+        cars.add(new ElectricCar("Tesla", "Model S", 2023, 350));
+        cars.add(new ElectricCar("Nissan", "Leaf", 2021, 240);
+        cars.add(new Car("Ford", "Mustang", 2022));
+
+        for(Car c: cars){
+            c.startEngine();
+        }
+
+    }
+}
