4960f7a4fff410fdc48ccfb0353509533591f154
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-11 16:37:57 +0530

    Create stream.java

---
+++ JAVA/Assignment | 10-02-25/stream.java
@@ -0,0 +1,16 @@
+package org.example.assignment;
+import java.io.*;
+public class stream {
+    public static void main(String [] args)
+    {
+        System.out.print("System.print.out!\n");
+        String name="Jack Sparrow";
+        System.out.print("Enter the title: "+ name);
+
+        System.out.println("Welcome to Black pearl");
+        System.out.println("Watch the "+name+" now on Netflix!");
+
+        System.err.print("Error: NoCodeFound");
+        System.err.println("Error: NoGo");
+    }
+}
