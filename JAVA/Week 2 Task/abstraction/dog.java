ede01891dbe808735f9bd9fb9b9de34a4c1045d0
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-11 15:47:28 +0530

    Create dog.java

---
+++ JAVA/Week 2 Task/abstraction/dog.java
@@ -0,0 +1,9 @@
+package org.example.assignment.abstracteg;
+
+public class Dog extends Animal{
+
+    @Override
+    void makeSound() {
+        System.out.println("The dog barks - Woof..Woof..!");
+    }
+}
