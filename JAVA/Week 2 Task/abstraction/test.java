d640bb3baee2c26a5e442eae5df0af989bcb8ef3
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-11 15:51:09 +0530

    Update test.java

--- JAVA/Week 2 Task/abstraction/test.java
+++ JAVA/Week 2 Task/abstraction/test.java
@@ -3,9 +3,9 @@ package org.example.assignment.abstracteg;
 public class Testm {
     public static void main(String[] args){
         Dog d=new Dog();
-        Cat o=new Cat();
+        Cat c=new Cat();
 
         d.makeSound();
-        o.makeSound();
+        c.makeSound();
     }
 }
