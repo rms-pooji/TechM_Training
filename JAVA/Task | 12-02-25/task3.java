713a2899599871671ed5e9ddac965edd884e19e7
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-12 21:44:14 +0530

    Update task3.java

--- JAVA/Task | 12-02-25/task3.java
+++ JAVA/Task | 12-02-25/task3.java
@@ -1,54 +1,23 @@
 package multithreading;
-import java.io.*;
 
-class Employee implements Serializable {
-    private int Eno;
-    private final String Ename;  // Marked as final
-    private transient String email;  // Marked as transient to exclude it from serialization
+public class P_Task1_Thread1 extends Thread {
 
-    // Constructor
-    public Employee(int Eno, String Ename, String email) {
-        this.Eno = Eno;
-        this.Ename = Ename;
-        this.email = email;
-    }
-
-    // Getters
-    public int getEno() {
-        return Eno;
-    }
-
-    public String getEname() {
-        return Ename;
-    }
-
-    public String getEmail() {
-        return email;
+    public void run(){
+        System.out.println("From ThreadOne!");
+        System.out.println("From ThreadOne!");
+        System.out.println("From ThreadOne!");
+        System.out.println(10/0);
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
-        // Create an Employee object (this is where the Employee class is used)
-        Employee emp = new Employee(01, "Johnny depp", "johnny.depp@example.com");
+class Sample {
+    public static void main(String[] args){
+        P_Task1_Thread1 p1= new P_Task1_Thread1();
+       // l1.start();
+        p1.run();
 
-        // Serialize the object (excluding the email field)
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("employee.ser"))) {
-            out.writeObject(emp);  // Serialize the Employee object
-            System.out.println("Employee object serialized successfully.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Deserialize the object and display the values
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("employee.ser"))) {
-            Employee deserializedEmp = (Employee) in.readObject();  // Deserialize the Employee object
-            System.out.println("Deserialized Employee:");
-            System.out.println("Eno: " + deserializedEmp.getEno());
-            System.out.println("Ename: " + deserializedEmp.getEname());
-            System.out.println("Email: " + deserializedEmp.getEmail());  // This will be null because it's transient
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        System.out.println(10/0);
     }
 }
+
+      
