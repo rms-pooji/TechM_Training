accf9a98baf3da7d36778f8c74736fe818d8c79d
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-14 11:15:10 +0530

    Create task2.js

---
+++ JAVA/Javascript | 14-02-25/task2.js
@@ -0,0 +1,8 @@
+var number = -7; // You can change this value to any number
+if (number > 0) {
+  console.log("The number", number, "is positive.");
+} else if (number < 0) {
+  console.log("The number", number, "is negative.");
+} else {
+  console.log("The number is zero.");
+}
