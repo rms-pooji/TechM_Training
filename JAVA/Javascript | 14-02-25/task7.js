7d54c22102c614a587638c7c6f055a6acea2d505
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-14 12:59:06 +0530

    Create task7.js

---
+++ JAVA/Javascript | 14-02-25/task7.js
@@ -0,0 +1,8 @@
+let Num=parseInt(prompt("Enter the number :"))
+if (Num %12==0){
+    console.log("The Number is divisible by 12")
+}
+else{
+    console.log("The number is not divisible by 12")
+    
+}
