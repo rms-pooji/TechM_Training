c70335fb17e49d94ee6e5569728abf0df91995fc
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-14 11:17:54 +0530

    Create task4.js

---
+++ JAVA/Javascript | 14-02-25/task4.js
@@ -0,0 +1,5 @@
+// Prompt the user to enter their address
+let userAddress = prompt("Please enter your address:");
+
+// Display the entered address in an alert
+alert("Your address is: " + userAddress);
