974d7aecfb9e1939378ad072566318bbd461a490
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-14 11:14:26 +0530

    Create task1.js

---
+++ JAVA/Javascript | 14-02-25/task1.js
@@ -0,0 +1,3 @@
+let number = 5; // You can change this value to any number
+let cube = number ** 3; // Calculating the cube of the number
+console.log("The cube of", number, "is", cube); // Display the result
