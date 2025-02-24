d5968d4a771adf2bd2711e2ed7718b5a1dce79dc
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-24 14:37:39 +0530

    Create Java.js

---
+++ JAVA/React js | 24-02-2025/Task_6/Java.js
@@ -0,0 +1,13 @@
+// src/Java.js
+import React from 'react';
+
+function Java() {
+  return (
+    <div className="topic">
+      <h2>Java</h2>
+      <p>Java is a popular, class-based, object-oriented programming language.</p>
+    </div>
+  );
+}
+
+export default Java;
