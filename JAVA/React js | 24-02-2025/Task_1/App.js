9cf39c67b6d0342def1b087438c963d0cbad4588
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-24 13:45:47 +0530

    Create App.js

---
+++ JAVA/React js | 24-02-2025/Task_1/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h2>This is an h2 tag inside a div</h2>
+      </div>
+      <div>
+        <h4>This is an h4 tag inside a div</h4>
+      </div>
+      <div>
+        <p>This is a paragraph tag inside a div</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
