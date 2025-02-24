1ad9147b2e393a60eadc79eb2952ba3da249ca75
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-24 14:35:59 +0530

    Create App.js

---
+++ JAVA/React js | 24-02-2025/Task_6/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Training from './Training'; // Import the Training component
+
+function App() {
+  return (
+    <div className="App">
+      <Training />
+    </div>
+  );
+}
+
+export default App;
