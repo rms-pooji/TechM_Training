d91ac4183f42586e183c0c50fbe439c6befa9e69
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-24 14:15:21 +0530

    Create App.js

---
+++ JAVA/React js | 24-02-2025/Task_3/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Course from './Course'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Course name="Information Technology" /> {/* Passing 'Information Technology' as the name prop */}
+    </div>
+  );
+}
+
+export default App;
