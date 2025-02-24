8db01a2330398051bf19b75d81711bc51b238148
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-24 14:39:31 +0530

    Create ReactComponent.js

---
+++ JAVA/React js | 24-02-2025/Task_6/ReactComponent.js
@@ -0,0 +1,13 @@
+// src/ReactComponent.js
+import React from 'react';
+
+function ReactComponent() {
+  return (
+    <div className="topic">
+      <h2>React</h2>
+      <p>React is a JavaScript library for building user interfaces, developed by Facebook.</p>
+    </div>
+  );
+}
+
+export default ReactComponent;
