4475a737c1d88473da439a43eddd3976d2e8f143
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-24 09:26:48 +0530

    Create task_16.java

---
+++ JAVA/CoreJava_Tasks/TASK_7/task_16.java
@@ -0,0 +1,26 @@
+package org.example.assignment.corejava.Task7;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class task16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> list = new LinkedList<>();
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+        System.out.println("Enter the element to add to the list: ");
+        int element=sc.nextInt();
+
+        list.addFirst(element);
+        list.addLast(element);
+
+        System.out.print("Updated list: "+list);
+    }
+}
