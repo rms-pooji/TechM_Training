872d78f2caef4c019b3f0baf48740e8d75cfb38a
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-24 14:35:19 +0530

    Create index.js

---
+++ JAVA/React js | 24-02-2025/Task_6/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css'; // Import the styles
+import App from './App'; // Import the main App component
+
+// Render the App component inside the root div in index.html
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
