75aec3b35c079fe491102e9f06d78e815dac5e05
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-24 08:01:50 +0530

    Update task_5.java

--- JAVA/CoreJava_Tasks/TASK_1/task_5.java
+++ JAVA/CoreJava_Tasks/TASK_1/task_5.java
@@ -1,11 +1,23 @@
-import java.util.Arrays;
-
-public class ArrayCopyRangeExample {
+public class AlternateCapitalization {
     public static void main(String[] args) {
-        // Original array
-        int[] originalArray = {10, 20, 30, 40, 50, 60, 70, 80};
-        int[] copiedArray = Arrays.copyOfRange(originalArray,2,6);
-        System.out.println("Original Array: " + Arrays.toString(originalArray));
-        System.out.println("Copied Array (Index 2 to 5): " + Arrays.toString(copiedArray));
+        String input = "hello world";
+        String result = capitalizeAlternate(input);
+        System.out.println("Original String: " + input);
+        System.out.println("Modified String: " + result);
+    }
+
+    public static String capitalizeAlternate(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (i % 2 == 0) {
+                sb.append(Character.toUpperCase(ch)); 
+            } else {
+                sb.append(Character.toLowerCase(ch)); 
+            }
+        }
+
+        return sb.toString();
     }
 }
