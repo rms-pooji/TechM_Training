116553bf892ff83ba8bfcf28c90e5edb4d18d2ac
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-24 14:40:17 +0530

    Create Angular.js

---
+++ JAVA/React js | 24-02-2025/Task_6/Angular.js
@@ -0,0 +1,13 @@
+// src/Angular.js
+import React from 'react';
+
+function Angular() {
+  return (
+    <div className="topic">
+      <h2>Angular</h2>
+      <p>Angular is a platform and framework for building single-page client applications using HTML and TypeScript.</p>
+    </div>
+  );
+}
+
+export default Angular;
