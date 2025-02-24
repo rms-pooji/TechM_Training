0029a9455188f4d556e8ed963750a2476d4ccbc2
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-24 14:38:23 +0530

    Create Javascript.js

---
+++ JAVA/React js | 24-02-2025/Task_6/Javascript.js
@@ -0,0 +1,13 @@
+// src/Javascript.js
+import React from 'react';
+
+function Javascript() {
+  return (
+    <div className="topic">
+      <h2>JavaScript</h2>
+      <p>JavaScript is a dynamic programming language used for web development.</p>
+    </div>
+  );
+}
+
+export default Javascript;
