fcba26e47cc2cd1a30dce0214107c554e4b39df0
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-28 08:08:21 +0530

    Create Lavender.jsx

---
+++ JAVA/React js | 27-02-25/Five Components Task/Lavender.jsx
@@ -0,0 +1,18 @@
+import react from "react";
+
+const Lavender = () => {
+  return (
+    <>
+      <div>
+        <h2>'Lavender - A whisper of serenity!'</h2>
+        <p>
+          Lavender, the flower of serenity and grace, symbolizes peace, purity,
+          and devotion. Its calming scent soothes the soul, while its soft
+          purple hue represents elegance and tranquility. A timeless emblem of
+          inner harmony and strength.
+        </p>
+      </div>
+    </>
+  );
+};
+export default Lavender;
