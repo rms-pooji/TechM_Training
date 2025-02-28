5bec401a1b11663fb0c78166f796ee6d9e48ba0d
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-28 08:11:24 +0530

    Create main.js

---
+++ JAVA/React js | 27-02-25/Unique id Task/main.js
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import ABComponent2 from "./assets/FlowerShop/s.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+      <ABComponent2 />
+  </StrictMode>
+);
