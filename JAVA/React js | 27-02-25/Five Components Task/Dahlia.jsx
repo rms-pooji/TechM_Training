a14eaaea47ba4f9e76bd08831c5e715f542a1678
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-28 08:06:00 +0530

    Create Dahlia.jsx

---
+++ JAVA/React js | 27-02-25/Five Components Task/Dahlia.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Dahlia = () => {
+  return (
+    <>
+      <div>
+        <h2>'Dahlia – Elegance in full bloom!'</h2>
+        <p>
+          Dahlia is a symbol of grace, strength, and everlasting elegance. With
+          its vibrant petals and intricate beauty, it stands tall, blooming in
+          resilience and charm.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Dahlia;
