3d231c2ad1c363bcb236c3aa79f3dd6d7296e1f6
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-28 08:09:39 +0530

    Create Sunflower.jsx

---
+++ JAVA/React js | 27-02-25/Five Components Task/Sunflower.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+
+const Sunflower = () => {
+  return (
+    <>
+      <div>
+        <h2>'Sunflower - Bright and bold!'</h2>
+        <p>
+          The sunflower, with its golden petals reaching for the sun, symbolizes
+          warmth, happiness, and unwavering faith. It stands tall, representing
+          loyalty and resilience, always turning toward the light. A symbol of
+          hope and positivity, the sunflower reminds us to embrace joy and
+          follow our dreams with confidence.{" "}
+        </p>
+      </div>
+    </>
+  );
+};
+export default Sunflower;
