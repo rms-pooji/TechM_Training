8b50deeeb2bc16bdb13550bca1786e3835428a5a
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-28 08:06:44 +0530

    Create Iris.jsx

---
+++ JAVA/React js | 27-02-25/Five Components Task/Iris.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Iris = () => {
+  return (
+    <>
+      <div>
+        <h2>'Iris - A bloom of wisdom and hope!'</h2>
+        <p>
+          Iris is a flower of wisdom, faith, and courage. Its petals unfold like
+          dreams waiting to bloom. Standing tall, it embraces the light with
+          grace. A symbol of hope in every season of life. Bloom like the iris,
+          strong and full of promise.
+        </p>
+      </div>
+    </>
+  );
+};
+export default Iris;
