fbccd4744910fd5bc68a9aac5293a2ef20f9de8e
Poojitha S <117002230+rms-pooji@users.noreply.github.com>
2025-02-28 07:58:36 +0530

    Create C1.jsx

---
+++ JAVA/React js | 27-02-25/Components/C1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import C2 from "./f1/C2"; 
+
+const C1 = () => {
+  return (
+    <div>
+      <h1>This is component C1!!</h1>
+      <C2 />
+    </div>
+  );
+};
+
+export default C1;
